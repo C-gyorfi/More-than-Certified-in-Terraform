da0867868a131d66ab002d8aaae54a65ce233e37
csabagyorfi <csaba@madetech.com>
2021-11-07 16:14:07 +0000

    Random resource and count

--- main.tf
+++ main.tf
@@ -13,18 +13,34 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  length = 4
+  special = false
+  upper = false
+}
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = join("-", ["nodered", random_string.random.result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
+  }
+}
+
+resource "docker_container" "nodered_container_multiple" {
+  count = 2
+  name  = "nodered_${count.index}"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    # external = 1880
   }
 }
 
 output "IP_address" {
-  value       = docker_container.nodered_container.ip_address
-  description = "the ip address of the container"
+  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  description = "the ip address and extrenal port of the container"
 }
 
 output "container_name" {
