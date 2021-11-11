35b5871810fef5faa9cc41b0870924fae4edfe5e
csabagyorfi <csaba@madetech.com>
2021-11-11 22:52:27 +0000

    Validation of a variable

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ variable "ext_port" {
 variable "int_port" {
   type = number
   default = 1880
+
+  validation {
+    condition = var.int_port <= 65535 && var.int_port > 0
+    error_message = "The internal port must be in the valid port range 0 - 65535."
+  }
 }
 variable "countainer_count" {
   type = number
