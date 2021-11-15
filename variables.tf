188c54914e07da113a8f82c16b7a5630961d3055
csabagyorfi <csaba@madetech.com>
2021-11-15 22:08:57 +0000

    List validation

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "ext_port" {
   type = list
+
+  validation {
+    condition = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 1
+    error_message = "The external port must be in the valid port range 0 - 65535."
+  }
 }
 
 variable "int_port" {
