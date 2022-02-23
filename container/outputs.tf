2db4e7174be3e2bdc2571061fe3809fa1578eda9
csabagyorfi <csaba@madetech.com>
2022-02-23 16:43:45 +0000

    Use map transoformation to output IP addresses for each container

--- container/outputs.tf
+++ container/outputs.tf
@@ -1,10 +1,5 @@
-output "IP_address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-  description = "the ip address and extrenal port of the container"
-  sensitive   = true
-}
-
-output "container_name" {
-  value       = docker_container.nodered_container[*].name
-  description = "the name of the container"
+output "application_access" {
+  value = {
+    for x in docker_container.container[*]: x.name => join(":", [x.ip_address], x.ports[*]["external"])
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
-output "IP_address" {
-  value       = module.container["nodered"].IP_address
-  description = "the ip address and extrenal port of the container"
-  sensitive   = true
-}
-
-output "container_name" {
-  value       = module.container["nodered"].container_name
-  description = "the name of the container"
+output "application_access" {
+  value = module.container
+  description = "The name and socket for all application"
 }
