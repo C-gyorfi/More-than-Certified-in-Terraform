0fb4ff1ccd6cf3d9182663d478bc40bc0a5af0ed
csabagyorfi <csaba@madetech.com>
2022-05-08 18:22:33 +0100

    Update all deplpyment volumes

--- locals.tf
+++ locals.tf
@@ -1,17 +1,21 @@
 locals {
   deployment = {
-    # nodered = {
-    #   countainer_count = length(var.ext_port["nodered"][terraform.workspace])
-    #   int_port = var.int_port
-    #   ext_port = var.ext_port["nodered"][terraform.workspace]
-    #   container_path = "/data"
-    # }
-    # influxdb = {
-    #   countainer_count = length(var.ext_port["influxdb"][terraform.workspace])
-    #   int_port = 8086
-    #   ext_port = var.ext_port["influxdb"][terraform.workspace]
-    #   container_path = "/var/lib/influxdb"
-    # }
+    nodered = {
+      countainer_count = length(var.ext_port["nodered"][terraform.workspace])
+      int_port         = var.int_port
+      ext_port         = var.ext_port["nodered"][terraform.workspace]
+      volumes = [
+        { container_path_each = "/data" },
+      ]
+    }
+    influxdb = {
+      countainer_count = length(var.ext_port["influxdb"][terraform.workspace])
+      int_port         = 8086
+      ext_port         = var.ext_port["influxdb"][terraform.workspace]
+      volumes = [
+        { container_path_each = "/var/lib/influxdb" },
+      ]
+    }
     grafana = {
       countainer_count = length(var.ext_port["grafana"][terraform.workspace])
       int_port         = 3000
